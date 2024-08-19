0510553bd5169cbb638863d3d58dcccdaaf4e225
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-19 17:12:18 +0200

    other factory product

--- DesignPattern.FactoryMethod/Controllers/ProductDetailController.cs
+++ DesignPattern.FactoryMethod/Controllers/ProductDetailController.cs
@@ -9,12 +9,15 @@ namespace DesignPattern.FactoryMethod.Controllers
         {
             // Factories
             LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.21m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.30m, 15);
 
             // Products
             var localEarn = localEarnFactory.GetEarn();
+            var totalForeign = foreignEarnFactory.GetEarn();
 
             // Total
             ViewBag.totalLocal = total + localEarn.Earn(total);
+            ViewBag.foreignEarn = total + totalForeign.Earn(total);
 
             return View();
         }
---
+++ FactoryMethod.Tools/Earn/ForeignEarn.cs
@@ -0,0 +1,19 @@
+﻿namespace FactoryMethod.Tools.Earn
+{
+    public class ForeignEarn : IEarn
+    {
+        private decimal _percentage;
+        private decimal _extraTax;
+
+        public ForeignEarn(decimal percentage, decimal extraTax)
+        {
+            _percentage = percentage;
+            _extraTax = extraTax;
+        }
+
+        public decimal Earn(decimal amount)
+        {
+            return (_percentage * amount) + _extraTax;
+        }
+    }
+}
