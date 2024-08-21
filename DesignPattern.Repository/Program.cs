327851a3f68598be85955b9f52cc5d9c6353496e
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-21 16:59:43 +0200

    create generic interface

--- DesignPattern.Repository/Program.cs
+++ DesignPattern.Repository/Program.cs
@@ -1,23 +1,7 @@
-﻿using DesignPattern.Repository.Models;
-using DesignPattern.Repository.Repository;
-
-public static class Program
+﻿public static class Program
 {
     public static void Main()
     {
-        using (var context = new RepositoryPatternContext())
-        {
-            var beerRepository = new BeerRepository(context);
-            var beer = new Beer();
-            beer.Name = "Corona";
-            beer.Style = "Pilsner";
-            beerRepository.Add(beer);
-            beerRepository.Save();
 
-            foreach (var item in beerRepository.Get())
-            {
-                Console.WriteLine(item.Name);
-            }
-        }
     }
 }
