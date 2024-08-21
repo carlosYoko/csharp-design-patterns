668463f7f67bb96ad72db447ef4022010f85d9c9
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-21 20:57:55 +0200

    scaffolding and references

---
+++ DesignPattern.Repository.Models/Data/RepositoryPatternContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Repository.Models.Data;
+
+public partial class RepositoryPatternContext : DbContext
+{
+    public RepositoryPatternContext()
+    {
+    }
+
+    public RepositoryPatternContext(DbContextOptions<RepositoryPatternContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beer> Beers { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=RepositoryPattern;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beer>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Beer__3214EC07992D3AD5");
+
+            entity.ToTable("Beer");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Style)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Brand__3214EC0761799F2E");
+
+            entity.ToTable("Brand");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
