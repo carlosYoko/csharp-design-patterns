b10d4daa0340a7f46673943ab1b6b18fd20446b8
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-21 21:03:10 +0200

    added asp project

--- DesignPattern.Repository/Program.cs
+++ DesignPattern.Repository/Program.cs
@@ -1,26 +1,27 @@
-﻿using DesignPattern.Repository.Models;
-using DesignPattern.Repository.Repository;
+var builder = WebApplication.CreateBuilder(args);
 
-public static class Program
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    public static void Main()
-    {
-        using (var context = new RepositoryPatternContext())
-        {
-            var beerRepository = new Repository<Beer>(context);
-            var beer = new Beer() { Name = "Chuff", Style = "Belgium" };
-            beerRepository.Add(beer);
-            beerRepository.Save();
-
-            foreach (var item in beerRepository.Get()) { Console.WriteLine(item.Name); }
-
-
-            var brandRepository = new Repository<Brand>(context);
-            var brand = new Brand() { Name = "Damm" };
-            brandRepository.Add(brand);
-            brandRepository.Save();
-
-            foreach (var item in brandRepository.Get()) { Console.WriteLine(item.Name); }
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
