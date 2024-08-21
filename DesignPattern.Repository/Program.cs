f2312ae77d33bffac68dbe6c66eadaa6b0113c4e
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-21 17:20:56 +0200

    using repository

--- DesignPattern.Repository/Program.cs
+++ DesignPattern.Repository/Program.cs
@@ -1,7 +1,21 @@
-﻿public static class Program
+﻿using DesignPattern.Repository.Models;
+using DesignPattern.Repository.Repository;
+
+public static class Program
 {
     public static void Main()
     {
+        using (var context = new RepositoryPatternContext())
+        {
+            var beerRepository = new Repository<Beer>(context);
+            var beer = new Beer() { Name = "Chuff", Style = "Belgium" };
+            beerRepository.Add(beer);
+            beerRepository.Save();
 
+            foreach (var item in beerRepository.Get())
+            {
+                Console.WriteLine(item.Name);
+            }
+        }
     }
 }
