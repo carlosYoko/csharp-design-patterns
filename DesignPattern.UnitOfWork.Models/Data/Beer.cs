3c67df6bbf8946deade380a064c3cd22f812120c
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-22 18:30:30 +0200

    create structure project and scaffolding db

---
+++ DesignPattern.UnitOfWork.Models/Data/Beer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPattern.UnitOfWork.Models.Data;
+
+public partial class Beer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Style { get; set; } = null!;
+
+    public Guid BeerId { get; set; }
+
+    public virtual Brand BeerNavigation { get; set; } = null!;
+}
---
+++ DesignPattern.UnitOfWork.Models/Data/UnitOfWorkContext.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.UnitOfWork.Models.Data;
+
+public partial class UnitOfWorkContext : DbContext
+{
+    public UnitOfWorkContext()
+    {
+    }
+
+    public UnitOfWorkContext(DbContextOptions<UnitOfWorkContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beer> Beers { get; set; }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=UnitOfWork;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beer>(entity =>
+        {
+            entity.ToTable("Beer");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Style)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.BeerNavigation).WithMany(p => p.Beers)
+                .HasForeignKey(d => d.BeerId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Beer_Brand");
+        });
+
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.ToTable("Brand");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
