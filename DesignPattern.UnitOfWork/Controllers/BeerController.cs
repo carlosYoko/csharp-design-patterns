b136b11cce50aa012c6a455dfde52cefffd77248
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-22 22:27:33 +0200

    added view model and table

---
+++ DesignPattern.UnitOfWork/Controllers/BeerController.cs
@@ -0,0 +1,28 @@
+﻿using DesignPattern.UnitOfWork.Models.ViewModels;
+using DesignPattern.UnitOfWork.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.UnitOfWork.Controllers
+{
+    public class BeerController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public BeerController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<BeerViewModel> beers = from d in _unitOfWork.Beers.Get()
+                                               select new BeerViewModel
+                                               {
+                                                   Id = d.Id,
+                                                   Name = d.Name,
+                                                   Style = d.Style,
+                                               };
+            return View("Index", beers);
+        }
+    }
+}
