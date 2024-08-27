50364172952b81f2528f241e636e1f3b81f3450e
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-27 17:01:00 +0200

    added strategy classes

---
+++ DesignPattern.Strategy/Strategies/BeerStrategy.cs
@@ -0,0 +1,20 @@
+﻿using DesignPattern.UnitOfWork.Models.Data;
+using DesignPattern.UnitOfWork.Models.ViewModels;
+using DesignPattern.UnitOfWork.Repository;
+
+namespace DesignPattern.Strategy.Strategies
+{
+    public class BeerStrategy : IBeerStrategy
+    {
+        public void Add(FormBeerViewModel beerVm, IUnitOfWork unitOfWork)
+        {
+            var beer = new Beer();
+            beer.Name = beerVm.Name;
+            beer.Style = beerVm.Style;
+            beer.BrandId = (Guid)beerVm.BrandId;
+
+            unitOfWork.Beers.Add(beer);
+            unitOfWork.Save();
+        }
+    }
+}
---
+++ DesignPattern.Strategy/Strategies/BeerWithBrand.cs
@@ -0,0 +1,25 @@
+﻿using DesignPattern.UnitOfWork.Models.Data;
+using DesignPattern.UnitOfWork.Models.ViewModels;
+using DesignPattern.UnitOfWork.Repository;
+
+namespace DesignPattern.Strategy.Strategies
+{
+    public class BeerWithBrandStrategy : IBeerStrategy
+    {
+        public void Add(FormBeerViewModel beerVm, IUnitOfWork unitOfWork)
+        {
+            var beer = new Beer();
+            beer.Name = beerVm.Name;
+            beer.Style = beerVm.Style;
+
+            var brand = new Brand();
+            brand.Name = beerVm.OtherBrand;
+            brand.Id = Guid.NewGuid();
+            beer.BrandId = brand.Id;
+
+            unitOfWork.Brands.Add(brand);
+            unitOfWork.Beers.Add(beer);
+            unitOfWork.Save();
+        }
+    }
+}
