ded07f032c9f298ba4de451950976f398239336e
Carlos Gimenez Berral <carlos.gimenez@external.wolterskluwer.com>
2024-08-27 16:53:52 +0200

    added strategy interface

---
+++ DesignPattern.Strategy/Strategies/IBeerStrategy.cs
@@ -0,0 +1,10 @@
+﻿using DesignPattern.UnitOfWork.Models.ViewModels;
+using DesignPattern.UnitOfWork.Repository;
+
+namespace DesignPattern.Strategy.Strategies
+{
+    public interface IBeerStrategy
+    {
+        public void Add(FormBeerViewModel beerVm, IUnitOfWork unitOfWork);
+    }
+}
